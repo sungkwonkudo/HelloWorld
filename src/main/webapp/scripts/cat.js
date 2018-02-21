a433a3832e8be0356b3db2ce5b67e55c03bbbafe
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-21 11:36:28 -0500

    added js

--- src/main/webapp/scripts/cat.js
+++ src/main/webapp/scripts/cat.js
@@ -1,3 +1,13 @@
 /**
  * 
  */
+let catText = document.getElementById('cat').textContent
+let catDiv =document.getElementById('cat');
+
+
+catDiv.onmouseover = function(){
+	document.getElementById('cat').textContent="meow";
+}
+catDiv.onmouseout = function(){
+	document.getElementById('cat').textContent=catText;
+}
